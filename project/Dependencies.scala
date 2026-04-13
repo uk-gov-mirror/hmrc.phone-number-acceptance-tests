fa7d67b3a03993d23124b588277b1e81e705767a
lakshmipochampally <28435840+lakshmipochampally@users.noreply.github.com>
2026-04-13 12:06:32 +0100

    Updated the Dependencies uk.gov.hmrc.mongo: hmrc-mongo-test-play-30 from 2.7.0 to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "api-test-runner"          % "0.10.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"  % "2.7.0"
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"  % "2.12.0"
   ).map(_ % Test)
 }
